b316601025afa883da6ed3d25504fbbe56942c26
mene412 <103831272+mene412@users.noreply.github.com>
2022-11-17 16:50:43 +0100

    Update Date.h

--- Date.h
+++ Date.h
@@ -3,37 +3,36 @@
 
 #include <ostream>
 #include <string>
+#include <vector>
 
 enum class Month {
-	def, jan, feb, mar, apr, may, jun, jul, aug, sep, oct, nov, dec
+	jan = 1, feb, mar, apr, may, jun, jul, aug, sep, oct, nov, dec
 };
 
 class Date{
 	public:
 		Date(int y, Month m, int d, bool exist);
-		Date(bool exist);
 		class Invalid {};
 		int year(void) const {return year_;}
 		Month month(void) const {return month_;}
 		int day(void) const {return day_;}
 		bool exist(void) const {return exist_;}
-		bool is_date(void);
-		static bool leapyear(int y);
 		
-
+		bool is_date(void);
+		
+		bool leapyear(void);
+		
 		std::string str_copyright(void) const;
 		
 		std::string int_to_month(void) const;
 		std::vector<std::string> month_print_tbl_ = {"gennaio", "febbraio", "marzo", "aprile", "maggio", "giugno", "luglio", "agosto", "settembre", "ottobre", "novembre", "dicembre"};	
-
 	private:
 		int year_;
 		Month month_;
 		int day_;
-		bool exist_;		
+		bool exist_;	
+		
 };
-
-
 
 std::ostream& operator<<(std::ostream& os, const Date& d);
 
