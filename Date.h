20ca95b2760e67c02c63dc172cd000ac05e1789d
mene412 <103831272+mene412@users.noreply.github.com>
2022-11-20 10:58:11 +0100

    Update Date.h

--- Date.h
+++ Date.h
@@ -8,29 +8,29 @@ enum class Month {
 	def = 0, jan, feb, mar, apr, may, jun, jul, aug, sep, oct, nov, dec
 };
 
-class Date{
+class Date {
 	public:
+		// costruttori
 		Date(int day, Month month, int year, bool exist);
 		Date(bool exist);
+		// classe per la gestione degli errori
 		class Invalid {};
+		// metodi getter
 		int year(void) const {return year_;}
 		Month month(void) const {return month_;}
 		int day(void) const {return day_;}
 		bool exist(void) const {return exist_;}
-		
+		// metodi ausiliari
 		bool is_date(void);
-		
 		bool leapyear(void);
-		
 		std::string str_copyright(void) const;
-		
 		std::string month_to_int(void) const;
+		
 	private:
 		int year_;
 		Month month_;
 		int day_;
-		bool exist_;	
-		
+		bool exist_;			
 };
 
 std::ostream& operator<<(std::ostream& os, const Date& d);
